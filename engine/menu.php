3f24ded22c77ea2e8611511820aefe634171fbf4
Den107 <deniskrv@gmail.com>
2021-02-08 12:46:46 +0300

    очистил master

--- engine/menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-include ENGINE_DIR . "db.php";
-
-function getMenu(): array
-{
-   return query( "SELECT * FROM menu WHERE access = 1");
-}
--- public/photo.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once __DIR__ . "/../config/main.php";
-include_once ENGINE_DIR . "db.php";
-
-$id = (int) $_GET['id'];
